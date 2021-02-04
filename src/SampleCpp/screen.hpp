a1ab056c3c719e05a33dc809975c6348e5062536
Oğuz Karan <oguzkaran@csystem.org>
2021-02-04 15:41:32 +0300

    Rota-Cpp-Jan-2021

---
+++ src/SampleCpp/screen.hpp
@@ -0,0 +1,6 @@
+#ifndef SCREEN_H_
+#define SCREEN_H_
+
+enum class Color { Red, Green, Blue };
+
+#endif //SCREEN_H_
---
+++ src/SampleCpp/traffic.hpp
@@ -0,0 +1,6 @@
+#ifndef TRAFFIC_HPP_
+#define TRAFFIC_HPP_
+
+enum class TrafficLight { Yellow, Green, Red };
+
+#endif //TRAFFIC_HPP_
